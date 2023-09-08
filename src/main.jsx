b7016704c6f94b2ac15b23ce3eb4a2c9eae8cc0b
auttpon <auttaponcoe24@gmail.com>
2023-09-08 09:41:39 +0700

    vite-template

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Vite</h1>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
